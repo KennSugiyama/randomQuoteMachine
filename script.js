736153da54d895b99630ce8a9a0a174a24607e21
Kenn Sugiyama <kenn@kbsugiyama.com>
2017-10-23 16:28:29 -0700

    Create generateQuote, apiConnector and processRequest functions in script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-  alert("yes")
+$(document).ready(function() {
+  $("#generatorButton").on("click",() => generateQuote());
+
+  let generateQuote = function(){
+    let url="http://api.forismatic.com/api/1.0/?method=getQuote&lang=en"
+
+
+  }// generateQuote
+
+  function apiConnector(url) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET',url,true);
+    xhr.send();
+    xhr.addEventListener('readystatechange', processRequest,false);
+
+    function processRequest(e) {
+      if (xhr.readyState == 4 & xhr.status == 200) {
+        let response = JSON.parse(xhr.responseText);
+        return response
+      }
+    }
+  }// apiConnection
+
 })
